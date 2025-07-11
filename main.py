ef4637879ec14ea31e11921064eac5fda60fdfb1
Jessica Jones <jones.jessr@gmail.com>
2025-07-11 12:11:03 -0400

    Added one-time auth script that creates a cache file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from spotipy.oauth2 import SpotifyOAuth
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 def setup_logger( name, log_file, level=logging.INFO):
-    handler = logging.RotatingFileHandler(log_file, maxBytes = (1024*1024), backupCount = 1)
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes = (1024*1024), backupCount = 1)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
@@ -22,7 +22,6 @@ def setup_logger( name, log_file, level=logging.INFO):
     return logger
 
 discord_logger = setup_logger('discord', 'discord.log')
-auth_logger = setup_logger('auth', 'auth.log')
 
 # grabbing secrets from env file
 load_dotenv()
@@ -32,6 +31,7 @@ CHANNEL = os.getenv('DISCORD_CHANNEL_ID')
 SPOTIFYID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFYSECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 PLAYLISTID = os.getenv('SPOTIFY_PLAYLIST_ID')
+CACHEPATH = os.getenv('CACHE_PATH')
 
 # setting up discord object
 intents = discord.Intents.default()
@@ -42,21 +42,13 @@ client = discord.Client(intents=intents)
 
 # set up spotify object
 scope = "playlist-modify-public"
-redirect_uri = "http://localhost:3000/callback"
+redirect_uri = "http://localhost:8888/callback"
 auth_manager = SpotifyOAuth(client_id = SPOTIFYID,
                             client_secret = SPOTIFYSECRET,
                             redirect_uri = redirect_uri,
-                            scope = scope)
+                            scope = scope,
+                            cache_path = CACHEPATH)
 sp = spotipy.Spotify(auth_manager = auth_manager )
-
-if not auth_manager.get_cached_token():
-    print("Opening authorization URL in your browser...")
-    auth_url = auth_manager.get_authorize_url()
-    print(f"Please go to the following URL and authorize access: {auth_url}")
-    input("Press Enter after you've authorized the app...")
-
-# Now Spotipy is ready to use
-print("Authorization successful. You can start using Spotipy.")
 
 # takes a spotify url and adds song to defined playlist
 async def add_song_to_playlist(song_url, playlist_id, sp):
